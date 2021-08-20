2d9d802d4072ed6e317c379fc31341adba8075ec
KamiHitoe <sousaku_0912@yahoo.co.jp>
2021-08-20 19:52:12 +0900

    demo

---
+++ demo.rb
@@ -0,0 +1,16 @@
+
+colors = ['red', 'blue', 'cyan']
+p colors.class
+p colors.length
+p colors[0..1]
+
+colors[0] = 'white'
+p colors
+
+colors.push('gold')
+p colors
+
+colors << 'silver'
+p colors
+
+p colors.sort
