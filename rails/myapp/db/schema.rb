97c46f2fbea28b75236d4c414385e22718ec4262
Alice <sousaku_0912@yahoo.co.jp>
2021-08-24 21:39:21 +0900

    from ALICE

--- rails/myapp/db/schema.rb
+++ rails/myapp/db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_08_20_152818) do
 
-  create_table "memos", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
