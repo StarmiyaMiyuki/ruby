329ee61a1578d1403f5b35cfb39326f79d5fe103
KamiHitoe <sousaku_0912@yahoo.co.jp>
2021-08-25 11:02:37 +0900

    delete rails

--- rails/myapp/app/models/post.rb
+++
@@ -1,9 +0,0 @@
-class Post < ApplicationRecord
-    has_many :comments
-    # @post.comments
-    validates :title, presence: true,
-        length: { minimum:1 , message: 'Please write something !'}
-    validates :body, presence: true
-
-
-end
