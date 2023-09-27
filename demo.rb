af8fe89803f7b7302dcd05cba062839f9baf7203
Miyuki Starmiya <kennouske.ueda@stmn.co.jp>
2023-09-27 19:08:11 +0900

    m

--- demo.rb
+++ demo.rb
@@ -1,39 +1,5 @@
+require 'colorize'
 
-class Human
-
-  # accessor
-  attr_accessor :class_info
-  attr_accessor :name
-  attr_accessor :age
-
-  # class area
-  info = "this is class var"
-  # class var
-  @@class_info = "class info"
-
-  # instance area
-  def initialize(name, age)
-    # instance var
-    @name = name
-    @age = age
-  end
-
-  # class method
-  def self.bow()
-    puts "hello! it's class method"
-    puts "#{@@class_info}"
-  end
-
-  # instance method
-  def say_hello()
-  puts "my name is #{@name}, I'm #{@age}"
-  puts "#{@@class_info}"
-
-  end
-end
-
-puts Human.class_variable_get(:@@class_info)
-Human.bow()
-hitoe = Human.new('hitoe', 27)
-hitoe.say_hello()
-puts hitoe.name
+puts String.colors
+puts "This is blue".colorize(:blue)
+# puts "This is blue"
